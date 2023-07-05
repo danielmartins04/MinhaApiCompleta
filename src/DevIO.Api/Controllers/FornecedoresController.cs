6706b60709105805b0f433be43a527aad4432b60
Daniel Martins <freitasdaniel39@gmail.com>
2023-07-05 00:19:43 -0300

    creating fornecedorConrtroller

---
+++ src/DevIO.Api/Controllers/FornecedoresController.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using DevIO.Api.ViewModels;
+using DevIO.Business.Intefaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class FornecedoresController : MainController
+    {
+        private readonly IFornecedorRepository _fornecedorRepository;
+        private readonly IMapper _mapper;
+
+        public FornecedoresController(IFornecedorRepository fornecedor, IMapper mapper)
+        {
+            _fornecedorRepository = fornecedor;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<FornecedorViewModel>> ObterTodos()
+        {
+            var fornecedor = _mapper.Map<IEnumerable<FornecedorViewModel>>(await _fornecedorRepository.ObterTodos());
+            return fornecedor;
+        }
+    }
+}
